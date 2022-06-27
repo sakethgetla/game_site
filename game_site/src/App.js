493826c5803e695e256401536f6fa48138de7863
sakethgetla <saketh.getla@protonmail.com>
2022-06-27 11:20:20 +1000

    update

--- game_site/src/App.js
+++ game_site/src/App.js
@@ -1,81 +1,73 @@
 // import logo from './logo.svg';
 // import './App.css';
 
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   useNavigate,
+//   Link
+// } from "react-router-dom";
 
-
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useNavigate,
-  Link
-} from "react-router-dom";
-
-import PathFinder from './games/PathFind/PathFind';
+// import PathFinder from './games/PathFind/PathFind';
 // import Astar from './games/AI/Astar_canvas'
-import {default as Scene} from './games/AI/Astar_canvas'
-import { AppBar, ButtonGroup, Button } from '@mui/material';
+// import {default as Scene} from './games/AI/Astar_canvas'
+import { default as Scene } from './games/AI/phy'
+// import { AppBar, ButtonGroup, Button } from '@mui/material';
+import { AppBar, Box, Button, Card, CardContent, Link, CardMedia, CardActionArea, Grid, Toolbar, Typography, IconButton } from '@mui/material';
 
 
 var homePath = '/game_site'
 export default function App() {
-// useNavigate(homePath);
+  // useNavigate(homePath);
   return (
-    <Router>
-      <div>
-      {/* {useNavigate(homePath)} */}
-        <nav>
-          <ul>
-            <AppBar position='static'>
-              <ButtonGroup variant="" size="large">
 
-                <Link to={homePath + "/"}>
-                  <Button variant="contained">
-                    Home
-                  </Button>
-                </Link>
+    <>
+      {/* <Box sx={{ flexGrow: 1 }}> */}
+      {/* <Box > */}
+        <AppBar enableColorOnDark={true} position="static" color="primary">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              {/* <MenuIcon /> */}
+            </IconButton>
+            <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+              Saketh Getla
+            </Typography>
+            {/* <Button component={Link} to={'https://sakethgetla.github.io'} color="inherit"> */}
+            {/* <Button color="inherit" > */}
+            <Button color="inherit"
+              onClick={(e) => {
+                e.preventDefault();
+                window.location.href = "https://sakethgetla.github.io"
+              }} >
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Projects
+              </Typography>
+            </Button>
+            {/* <Button component={Link} to={'/about'} color="inherit"> */}
+            {/* <Button color="inherit"> */}
+            {/*   <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}> */}
+            {/*     About */}
+            {/*   </Typography> */}
+            {/* </Button> */}
+            {/* <Button component={Link} to={'/about'} variant="outlined" color="inherit" >About</Button> */}
+            {/* <Link to="/about" color="inherit"> */}
+            {/*   <Button variant="outlined" color="inherit" >About</Button> */}
+            {/* </Link> */}
+            {/* <Button color="inherit">Login</Button> */}
+          </Toolbar>
+        </AppBar>
+      {/* </Box> */}
 
-                <Link to={homePath + "/about"}>
-                  <Button variant="contained">
-                    about
-                  </Button>
-                </Link>
-                <Link to={homePath + "/users"}>
-                  <Button variant="contained">
-                    users
-                  </Button>
-                </Link>
 
-                <Link to={homePath + "/scene"}>
-                  <Button variant="contained">
-                    Scene
-                  </Button>
-                </Link>
-
-                <Link to={homePath + "/pathFinder"}>
-                  <Button variant="contained">
-                    Path finding
-                  </Button>
-                </Link>
-
-              </ButtonGroup>
-            </AppBar>
-
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Routes>
-          <Route path={"/"} element={<Home />} />
-          <Route path={homePath + "/"} element={<Home />} />
-          <Route path={homePath + "/about"} element={<About />} />
-          <Route path={homePath + "/users"} element={<Users />} />
-          <Route path={homePath + "/pathFinder"  }element={<PathFinder />} />
-          <Route path={homePath + "/scene"  }element={<Scene />} />
-        </Routes>
-      </div>
-    </Router>
+      <Scene />
+    </>
   );
 }
 
